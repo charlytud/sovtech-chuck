d21e76afe25f933a54b3277e5ebcaaa77b81822b
DESKTOP-NTU3D63\georges <charlytud@gmail.com>
2020-12-07 06:40:08 +0200

    update header layout

--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Header() {    
     return (
         <div className="app-header">
-            <h1>Hallo Sovtech</h1>
+            <h1>Hallo Sovtech!</h1>
             <p>
                 <ul>
                     <li>Full Stack Engineer Test - Chuck</li>
