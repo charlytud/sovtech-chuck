2b55bedcdb44edffae4238b16bc57d86c80c9f55
DESKTOP-NTU3D63\georges <charlytud@gmail.com>
2020-12-07 06:37:15 +0200

    create and style header component

---
+++ client/src/components/header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function Header() {    
+    return (
+        <div className="app-header">
+            <h1>Hallo Sovtech</h1>
+            <p>
+                <ul>
+                    <li>Full Stack Engineer Test - Chuck</li>
+                    <li>Candidate: Georges C. Takoudjou</li>
+                    <li>C/W: 071 618 1209</li>
+                </ul>
+            </p>
+        </div>
+    )
+}
--- client/src/components/home/Home.js
+++ client/src/components/home/Home.js
@@ -1,39 +1,12 @@
-import React from 'react';
-import { gql } from 'apollo-boost';
-import { useQuery } from 'react-apollo';
+import React, { Component } from 'react'
+import Header from '../header/Header';
 
-export default function Home() {
-const QUERY_RANDOM_JOKE = gql`
-    query GetRandomJoke {
-    random_joke {
-    value
-        }
-    }
-    `;
-
-const QUERY_CATEGORIES = gql`
-    query GetCategories {
-    categories {
-    name
-        }
-    }
-    `;
-
-    // const joke = useQuery(QUERY_RANDOM_JOKE);
-    // console.log(joke);
-
-    const categories = useQuery(QUERY_CATEGORIES);
-    console.log(categories);
-    
-    return (
-        <div className="app-home">
-            <h2>Hallo Sovtech</h2>
-
-            <div className="app-home__category-list">
-                <ul>
-                    <li></li>
-                </ul>
+export default class Home extends Component {
+    render() {
+        return (
+            <div>
+                <Header />
             </div>
-        </div>
-    )
+        )
+    }
 }
